0f6aa36e855acfb0281ea938ece0b463ec959fdb
vagrant <vagrant@rails-dev-box>
2013-11-04 22:43:50 +0000

    installed / configured pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,12 +15,12 @@ module ApplicationHelper
 	end
 
 
-	def will_paginate(collection_or_options = nil, options = 
+	def will_paginate(collection_or_options = nil, options = {})
 		if collection_or_options.is_a? Hash
 			options, collection_or_options = collection_or_options, nil
 		end
 		unless options[:renderer]
-			options = options.merge :renderer => BootstrapLinkRender
+			options = options.merge :renderer => BootstrapLinkRenderer
 		end
 
 		super *[collection_or_options, options].compact
