e4ffc2d873f04005562baa6a8c4b839fdc829488
vagrant <vagrant@rails-dev-box>
2013-11-11 19:45:52 +0000

    Created popular Posts and Users, eager loading w topics#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Bloccit::Application.routes.draw do
-  
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations" }
+  
+  resources :posts, only: [:index]
   resources :topics do 
-  	resources :posts, except: [:index] do 
+  	resources :posts, except: [:index], controller: "topics/posts" do 
       resources :comments, only: [:create, :destroy]
       match "/up-vote", to: "votes#up_vote", as: :up_vote
       match "/down-vote", to: "votes#down_vote", as: :down_vote 
@@ -10,9 +11,7 @@ Bloccit::Application.routes.draw do
     end
   end
 
-  resources :users, only: [:show]
-  match "users" => "welcome#index"
-
+  resources :users, only: [:show, :index]
   match "about" => "welcome#about", via: :get
   root to: "welcome#index"
 end
