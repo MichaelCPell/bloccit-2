d39aa89aba6b77e074526f20d0eb4022772114ca
vagrant <vagrant@rails-dev-box>
2013-11-13 04:09:32 +0000

    Ajax destroy on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js 
   
   def create
     @topic = Topic.find(params[:topic_id])
@@ -23,14 +24,19 @@ class CommentsController < ApplicationController
     @post = @topic.posts.find(params[:post_id])
 
     @comment = @post.comments.find(params[:id])
-
     authorize! :destroy, @comment, message: "You need to own this comment to delete"
+    
+    
+
     if @comment.destroy
       flash[:notice] = "Comment deleted"
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error deleting comment"
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |f| 
+      f.html { redirect_to [@topic, @topic] }
     end
   end
+  
 end
